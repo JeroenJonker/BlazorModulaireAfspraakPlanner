5118adcd30a4dff1318b99f67f2210495f53f12b
Jeroen Jonker <jeroenojonker@hotmail.nl>
2019-05-21 11:53:31 +0200

    Added OptionsView

---
+++ testcoreblazor.Client/Viewmodels/OptionsViewModel.cs
@@ -0,0 +1,25 @@
+﻿using BlazorAgenda.Services.Interfaces;
+using BlazorAgenda.Shared.Models;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorAgenda.Client.Viewmodels
+{
+    public class OptionsViewModel : ComponentBase
+    {
+        public List<Option> Options { get; set; }
+
+        [Inject] IStateService StateService { get; set; }
+
+        protected override void OnInit()
+        {
+            Options = new List<Option>();
+            Console.WriteLine(StateService.LoginUser.Organization.Name);
+            //Organization organization = StateService.LoginUser.Organization;
+            base.OnInit();
+        }
+    }
+}
