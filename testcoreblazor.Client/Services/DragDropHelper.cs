251cf10698bd7728965fc58c42a579469ff5e16a
Jeroen Jonker <jeroenojonker@hotmail.nl>
2019-06-11 15:37:18 +0200

    added dragdrophelper again

---
+++ testcoreblazor.Client/Services/DragDropHelper.cs
@@ -0,0 +1,11 @@
+﻿using BlazorAgenda.Shared.Models;
+using System.Collections.Generic;
+
+namespace BlazorAgenda.Client.Services
+{
+    public static class DragDropHelper
+    {
+        public static List<CalendarEvent> Items { get; set; } = new List<CalendarEvent>();
+        public static CalendarEvent Item { get; set; }
+    }
+}
