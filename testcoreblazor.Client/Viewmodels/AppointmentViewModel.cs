70a3ca0ff4b574d3e67d04ca17e881c51e5c6049
Stefan Bakker <s.bakker777@gmail.com>
2019-06-11 12:31:11 +0200

    Fix user selection

--- testcoreblazor.Client/Viewmodels/AppointmentViewModel.cs
+++ testcoreblazor.Client/Viewmodels/AppointmentViewModel.cs
@@ -18,6 +18,7 @@ namespace BlazorAgenda.Client.Viewmodels
         [Inject] public IEvent Event { get; set; }
         public List<Option> Options { get; set; } = new List<Option>();
         List<EventOption> ChosenOptions { get; set; } = new List<EventOption>();
+        protected List<User> SelectedJobUsers{ get; set; } = new List<User>();
         
         protected override async Task OnInitAsync()
         {
@@ -39,9 +40,21 @@ namespace BlazorAgenda.Client.Viewmodels
             
         }
 
-        public void SetEventJob()
+        public void SetEventJob(UIChangeEventArgs e)
         {
+            SelectedJobUsers.Clear();
+            Event.UserId = -1;
+            
+            Event.JobId = Int32.Parse(e.Value.ToString());
+            foreach (Job job in StateService.Organization.Job) {
+                Console.WriteLine(job.Id);
+            }
             Event.Job = StateService.Organization.Job.First(job => job.Id == Event.JobId);
+
+            foreach (User user in StateService.Organization.User.Join(Event.Job.UserJob, u => u.Id, uj => uj.UserId, (u, uj) => u))
+            {
+                SelectedJobUsers.Add(user);
+            }
         }
 
         public void SubmitEventOptions()
