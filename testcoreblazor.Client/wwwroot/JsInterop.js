e5007c125b91551d8000e0ec011fed4e6708fa6e
Stefan Bakker <s.bakker777@gmail.com>
2019-06-13 13:28:53 +0200

    add multiselect

---
+++ testcoreblazor.Client/wwwroot/JsInterop.js
@@ -0,0 +1,8 @@
+window.JsFunctions = {
+    initializeSelect2: function() {
+      // $('select').select2();
+      $('.select-multiple-option-type-component').select2({
+        placeholder: "Selecteer uw keuzes"
+      });
+    }
+  }
